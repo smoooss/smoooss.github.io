57237b4e95150c47fddc102830d9cb360479baa5
smoooss <136715716+smoooss@users.noreply.github.com>
2024-03-24 00:16:12 +0900

    Delete discography directory

--- discography/cont/script/sub2-2.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function(){
-
-    $('#main .contentwrap .cont.c2 ul li .title ul li').click(function(){
-
-        $('#main .contentwrap .cont.c2 > ul').css({'height':'265px'})
-    });
-
-});
