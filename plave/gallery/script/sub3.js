e1bfd904671b230030c68d52bf2ce00a6bee0cdf
smoooss <136715716+smoooss@users.noreply.github.com>
2024-03-24 00:41:08 +0900

    Add files via upload

---
+++ plave/gallery/script/sub3.js
@@ -0,0 +1,56 @@
+$(document).ready(function(){
+    
+    a=$(window).width()
+
+    if(a>=500){
+        $('#main .contentwrap ul li').mouseover(function(){
+            $(this).find('a').css({'opacity':'1'})
+        }).mouseout(function(){
+            $(this).find('a').css({'opacity':'0'})
+        })
+    }
+    
+    $('#main .contentwrap ul li').click(popup);
+    
+    let index = 0
+
+    function popup(){
+        index = $(this).index()+1;
+        let url = 'url(img/content'+index+'.jpg)';
+        
+        a=$(window).scrollTop()
+
+        $('#main .popup').fadeIn(200).css({marginTop:a});
+        $('#main .popup .popup-cont').css({'backgroundImage':url});
+        $('html,body').css({'overflow':'hidden'}).animate({scrollTop:a});
+    }
+
+
+    $('#main .popup .popup-btn ul li').eq(0).click(function(){
+        $('#main .popup').css({'display':'none'})
+        $('html,body').css({'overflow':'auto'})
+    });
+
+    $('#main .popup .popup-btn ul li').eq(1).click(function(){
+
+        index--;
+        let url = 'url(img/content'+index+'.jpg)';
+        $('#main .popup .popup-cont').css({'backgroundImage':url});
+
+        if(index==1){
+            index = 32;
+        }
+    });
+
+    $('#main .popup .popup-btn ul li').eq(2).click(function(){
+
+        index++;
+        let url = 'url(img/content'+index+'.jpg)';
+        $('#main .popup .popup-cont').css({'backgroundImage':url});
+        
+        if(index==31){
+            index = 0;
+        }
+    });
+
+});
