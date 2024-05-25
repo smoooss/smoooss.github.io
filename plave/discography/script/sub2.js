93115bdb0a70c2ab37e898c02dfb1bc746dd3bac
oh! sangmi <136715716+smoooss@users.noreply.github.com>
2024-05-25 23:42:19 +0900

    sub2.js

--- plave/discography/script/sub2.js
+++ plave/discography/script/sub2.js
@@ -1,13 +1,11 @@
 $(document).ready(function(){
     
-    a=$(window).width()
-
-    if(a>=500){
-        $('#main .contentwrap ul li').mouseover(function(){
-            $(this).find('a').css({'opacity':'1'})
+    $('#main .contentwrap ul li').mouseover(function(){
+        a=$('#main .contentwrap ul li').width()
+            
+            $(this).find('a').css({'height':a,'opacity':'1'})
         }).mouseout(function(){
             $(this).find('a').css({'opacity':'0'})
         })
-    }
 
 });
