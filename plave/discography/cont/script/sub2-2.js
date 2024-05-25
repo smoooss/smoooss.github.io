e6b5aaa8dba0421327d9eeae2dbd3d73f1f3c0ad
oh! sangmi <136715716+smoooss@users.noreply.github.com>
2024-05-25 23:37:58 +0900

    sub2-2.js

--- plave/discography/cont/script/sub2-2.js
+++ plave/discography/cont/script/sub2-2.js
@@ -5,4 +5,23 @@ $(document).ready(function(){
         $('#main .contentwrap .cont.c2 > ul').css({'height':'265px'})
     });
 
+    a=$('#main .contentwrap .cont.c4 ul li > img').height()
+            
+    $('#main .contentwrap .cont.c4 ul li').find('a').css({'height':a})
+        
+
+    $('#main .contentwrap .cont.c4 ul li').mouseover(function(){
+        $(this).find('a').css({'opacity':'1'})
+    }).mouseout(function(){
+        $(this).find('a').css({'opacity':'0'})
+    })
+
+    $(window).on('resize',function(){
+
+        b=$('#main .contentwrap .cont.c4 ul li > img').height()
+
+        $('#main .contentwrap .cont.c4 ul li').find('a').css({'height':b})
+        
+    });
+
 });
