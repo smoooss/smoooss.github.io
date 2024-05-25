2942cbb053624f01a5d68a3c5d48ef741bdcbfd1
oh! sangmi <136715716+smoooss@users.noreply.github.com>
2024-05-25 23:38:09 +0900

    sub2-1.js

--- plave/discography/cont/script/sub2-1.js
+++ plave/discography/cont/script/sub2-1.js
@@ -10,4 +10,23 @@ $(document).ready(function(){
         $('#main .contentwrap .cont.c2 > ul').css({'height':'380px'})
     });
 
+    a=$('#main .contentwrap .cont.c4 ul li > img').height()
+            
+    $('#main .contentwrap .cont.c4 ul li').find('a').css({'height':a})
+        
+
+    $('#main .contentwrap .cont.c4 ul li').mouseover(function(){
+        $(this).find('a').css({'opacity':'1'})
+    }).mouseout(function(){
+        $(this).find('a').css({'opacity':'0'})
+    })
+
+    $(window).on('resize',function(){
+
+        b=$('#main .contentwrap .cont.c4 ul li > img').height()
+
+        $('#main .contentwrap .cont.c4 ul li').find('a').css({'height':b})
+        
+    });
+
 });
