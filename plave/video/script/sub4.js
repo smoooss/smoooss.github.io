91f2f1cd69ce6655e0428301ad16a5f4a81c966c
oh! sangmi <136715716+smoooss@users.noreply.github.com>
2024-05-25 22:34:52 +0900

    sub4.js

--- plave/video/script/sub4.js
+++ plave/video/script/sub4.js
@@ -4,94 +4,68 @@ $(document).ready(function(){
         ignore.preventDefault();
     });
 
-    $('#main .contentwrap .imagewrap .image.i1 ul li').click(image1);
+
+    $('#main .contentwrap .menu ul li').click(menu);
 
     let index = 0
 
-    function image1(){
+    function menu(){
         index = $(this).index()+1;
-        $('#main .contentwrap .videowrap .video.v1 ul li').eq(index-1).fadeIn(300).siblings().fadeOut(100)
+        $('#main .contentwrap .menu ul li').eq(index-1).css({'fontWeight':'700','color':'#333333'}).siblings().css({'fontWeight':'500','color':'#bbbbbb'})
+        $('#main .contentwrap .content div').eq(index-1).fadeIn(300).siblings().fadeOut(100)
     }
 
-    $('#main .contentwrap .imagewrap .image.i2 ul li').click(image2);
-    
-    let idx = 0
-
-    function image2(){
-        idx = $(this).index()+1;
-        $('#main .contentwrap .videowrap .video.v2 ul li').eq(idx-1).fadeIn(300).siblings().fadeOut(100)
-    }
-
-    $('#main .contentwrap .imagewrap .image.i3 ul li').click(image3);
-    
-    let indx = 0
-
-    function image3(){
-        indx = $(this).index()+1;
-        $('#main .contentwrap .videowrap .video.v3 ul li').eq(indx-1).fadeIn(300).siblings().fadeOut(100)
-    }
-
-    $('#main .contentwrap .menu ul li').click(function(){
-        $('#main .contentwrap .menu ul li').css({'fontWeight':'500','color':'#bbbbbb'})
-        $(this).css({'fontWeight':'700','color':'#333333'})
-    });
-
-    $('#main .contentwrap .menu ul li').eq(0).click(function(){
-        $('#main .contentwrap .videowrap .video').fadeOut(100)
-        $('#main .contentwrap .videowrap .video.v1').fadeIn(300)
-        $('#main .contentwrap .imagewrap .image').fadeOut(100)
-        $('#main .contentwrap .imagewrap .image.i1').fadeIn(300)
-    });
-
-    $('#main .contentwrap .menu ul li').eq(1).click(function(){
-        $('#main .contentwrap .videowrap .video').fadeOut(100)
-        $('#main .contentwrap .videowrap .video.v2').fadeIn(300)
-        $('#main .contentwrap .imagewrap .image').fadeOut(100)
-        $('#main .contentwrap .imagewrap .image.i2').fadeIn(300)
-    })
-
-    $('#main .contentwrap .menu ul li').eq(2).click(function(){
-        $('#main .contentwrap .videowrap .video').fadeOut(100)
-        $('#main .contentwrap .videowrap .video.v3').fadeIn(300)
-        $('#main .contentwrap .imagewrap .image').fadeOut(100)
-        $('#main .contentwrap .imagewrap .image.i3').fadeIn(300)
-    })
-
-
-
-    a=$(window).width()
-    if(a<500){
-
-        $('#main .contentwrap .imagewrap .image ul li').click(function(){
-            b=$(window).scrollTop()
+    a=$('#main .contentwrap .content ul li > img').height()
             
-            $('#main .contentwrap .videowrap').css({'top':b,'display':'block'})
-            $('#main .contentwrap .videowrap .btn').css({'display':'block'})
-            $('html,body').css({'overflow':'hidden'})
+    $('#main .contentwrap .content ul li').find('a').css({'height':a})
+        
+
+    $('#main .contentwrap .content ul li').mouseover(function(){
+        $(this).find('a').css({'opacity':'1'})
+    }).mouseout(function(){
+        $(this).find('a').css({'opacity':'0'})
+    })
+
+
+    b=$(window).width()
+
+    if(b<500){
+
+        c=$('#main .contentwrap .content .music').height()
+        d=$('#main .contentwrap .content .cont').height()
+        e=$('#main .contentwrap .content .cover').height()
+        h=$('#main .contentwrap .content .stage').height()
+        g=$('#main .contentwrap .content .radio').height()
+        
+        h=200
+
+        $('#main').css({'height':c+h})
+
+
+        $('#main .contentwrap .menu ul li').eq(1).click(function(){
+            $('#main').css({'height':d+h})
         })
-        $('#main .contentwrap .videowrap .btn img').click(function(){
-            $('#main .contentwrap .videowrap').css({'display':'none'})
-            $('#main .contentwrap .videowrap .btn').css({'display':'none'})
-            $('html,body').css({'overflow':'auto'})
+
+        $('#main .contentwrap .menu ul li').eq(2).click(function(){
+            $('#main').css({'height':e+h})
         })
+
+        $('#main .contentwrap .menu ul li').eq(3).click(function(){
+            $('#main').css({'height':f+h})
+        })
+
+        $('#main .contentwrap .menu ul li').eq(4).click(function(){
+            $('#main').css({'height':g+h})
+        })
+
 
         $('.header .menu').click(function(){
-            $('#main .link').css({'bottom':'60px'})
+            $('#wrap').css({'height':'100vh'})
         });
         $('.close').click(function(){
-            $('#main .link').css({'bottom':'20px'})
+            $('#wrap').css({'height':'unset'})
         });
 
-    }else{
-        $('#main .contentwrap .imagewrap .image ul li').mouseover(function(){
-            $(this).find('a').css({'opacity':'1'})
-        }).mouseout(function(){
-            $(this).find('a').css({'opacity':'0'})
-        });
-
-        $('#main .contentwrap .imagewrap .image ul li').click(function(){
-            $(this).css({'opacity':'1'}).siblings().css({'opacity':'0.5'})
-        });
     }
 
     
