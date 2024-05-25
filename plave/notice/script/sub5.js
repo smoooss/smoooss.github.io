9db6a07083247990f6259a81bd5b80561f0aba78
oh! sangmi <136715716+smoooss@users.noreply.github.com>
2024-05-26 00:11:44 +0900

    sub5.js

--- plave/notice/script/sub5.js
+++ plave/notice/script/sub5.js
@@ -1,5 +1,6 @@
 $(document).ready(function(){
 
+
     a=$(window).width()
 
     if(a>=500){
@@ -11,7 +12,7 @@ $(document).ready(function(){
         function cont1(){
             index = $(this).index()+1;
 
-            $('html,body').animate({scrollTop:index*89.5},500)
+            $('html,body').animate({scrollTop:index*89.5-80},500)
 
             a=$(this).find('.more').hasClass('on')
             
@@ -52,55 +53,55 @@ $(document).ready(function(){
 
     $(window).on('scroll touchmove mousewheel',function(){
 
-        b=$(window).scrollTop()
+        e=$(window).scrollTop()
         d=$(window).width()
 
         if(d<500){
-            if(b>10){
+            if(e>10){
                 for(c=10; c<20; c++){
                     $('#main .contentwrap ul li').eq(c).fadeIn(500)
                 }
-            }if(b>200){
+            }if(e>200){
                 for(c=20; c<30; c++){
                     $('#main .contentwrap ul li').eq(c).fadeIn(500)
                 }
-            }if(b>600){
+            }if(e>600){
                 for(c=30; c<40; c++){
                     $('#main .contentwrap ul li').eq(c).fadeIn(500)
                 }
-            }if(b>1000){
+            }if(e>1000){
                 for(c=40; c<50; c++){
                     $('#main .contentwrap ul li').eq(c).fadeIn(500)
                 }
-            }if(b>1400){
+            }if(e>1400){
                 for(c=50; c<60; c++){
                     $('#main .contentwrap ul li').eq(c).fadeIn(500)
                 }
-            }if(b>1800){
+            }if(e>1800){
                 for(c=60; c<70; c++){
                     $('#main .contentwrap ul li').eq(c).fadeIn(500)
                 }
-            }if(b>2200){
+            }if(e>2200){
                 for(c=70; c<80; c++){
                     $('#main .contentwrap ul li').eq(c).fadeIn(500)
                 }
-            }if(b>2600){
+            }if(e>2600){
                 for(c=80; c<90; c++){
                     $('#main .contentwrap ul li').eq(c).fadeIn(500)
                 }
-            }if(b>3000){
+            }if(e>3000){
                 for(c=90; c<100; c++){
                     $('#main .contentwrap ul li').eq(c).fadeIn(500)
                 }
-            }if(b>3400){
+            }if(e>3400){
                 for(c=100; c<110; c++){
                     $('#main .contentwrap ul li').eq(c).fadeIn(500)
                 }
-            }if(b>3800){
+            }if(e>3800){
                 for(c=110; c<120; c++){
                     $('#main .contentwrap ul li').eq(c).fadeIn(500)
                 }
-            }if(b>4200){
+            }if(e>4200){
                 for(c=120; c<130; c++){
                     $('#main .contentwrap ul li').eq(c).fadeIn(500)
                 }
