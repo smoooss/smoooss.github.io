97327bf2128a27e9aaef6f182481e5e441a3bfd3
oh! sangmi <136715716+smoooss@users.noreply.github.com>
2024-10-13 20:12:07 +0900

    Add files via upload

--- plave/script/main.js
+++ plave/script/main.js
@@ -1,25 +1,25 @@
 $(document).ready(function(){
 
     $(window).on('scroll touchmove mousewheel, mousemove',function(){
-        $('#main .contentwrap .content .text').css({'opacity':'1'})
+        $('main .container .content .text').css({'opacity':'1'})
     });
 
     $('.menu').click(function(){
-        $('#nav').fadeIn(150)
+        $('nav').fadeIn(150)
         $('html,body').css({'overflow':'hidden'})
-        $('#footer').css({'position':'fixed'})
-        $('#footer .footer p').css({'color':'#333333'})
+        $('footer').css({'position':'fixed'})
+        $('footer .footer p').css({'color':'#333333'})
     });
     $('.close').click(function(){
-        $('#nav').fadeOut(100)
+        $('nav').fadeOut(100)
         $('html,body').css({'overflow':'auto'})
-        $('#footer').css({'position':'absolute'})
-        $('#footer .footer p').css({'color':'#ffffff'})
+        $('footer').css({'position':'absolute'})
+        $('footer .footer p').css({'color':'#ffffff'})
     });
 
     a=$(window).width()
     if(a<500){
-        $('#main .contentwrap .content .text').css({'opacity':'1'})
+        $('main .container .content .text').css({'opacity':'1'})
     }
 
 });
--- plave/script/sub.js
+++ plave/script/sub.js
@@ -1,29 +1,29 @@
 $(document).ready(function(){
 
     $(window).on('scroll touchmove mousewheel, mousemove',function(){
-        $('#main .contentwrap').css({'opacity':'1'})
+        $('main .container').css({'opacity':'1'})
     });
     
     $('.header .menu').click(function(){
-        $('#nav').fadeIn(150)
+        $('nav').fadeIn(150)
         $('html,body').animate({scrollTop:0},0).css({'overflow':'hidden'})
-        $('#footer').css({'position':'fixed'})
+        $('footer').css({'position':'fixed'})
     });
     $('.close').click(function(){
-        $('#nav').fadeOut(100)
+        $('nav').fadeOut(100)
         $('html,body').css({'overflow':'auto'})
-        $('#footer').css({'position':'relative'})
+        $('footer').css({'position':'relative'})
     });
 
     a=$(window).width()
     if(a<500){
-        $('#main .contentwrap').css({'opacity':'1'})
+        $('main .container').css({'opacity':'1'})
 
         $('.header .menu').click(function(){
-            $('#main .link').css({'position':'fixed'})
+            $('main .link').css({'position':'fixed'})
         });
         $('.close').click(function(){
-            $('#main .link').css({'position':'absolute'})
+            $('main .link').css({'position':'absolute'})
         });
     }
 
