2b7aac1b929ad6ee0d5df9d8cea29af995389b31
oh! sangmi <136715716+smoooss@users.noreply.github.com>
2024-10-13 20:15:13 +0900

    Add files via upload

--- plave/gallery/script/sub3.js
+++ plave/gallery/script/sub3.js
@@ -1,16 +1,18 @@
 $(document).ready(function(){
     
+    const totalItems = $('.wrapper ul li').length;
+
     a=$(window).width()
 
     if(a>=500){
-        $('#main .contentwrap ul li').mouseover(function(){
+        $('main .container ul li').mouseover(function(){
             $(this).find('a').css({'opacity':'1'})
         }).mouseout(function(){
             $(this).find('a').css({'opacity':'0'})
         })
     }
     
-    $('#main .contentwrap ul li').click(popup);
+    $('main .container ul li').click(popup);
     
     let index = 0
 
@@ -18,35 +20,35 @@ $(document).ready(function(){
         index = $(this).index()+1;
         let url = 'url(img/content'+index+'.jpg)';
 
-        $('#main .popup').css({'display':'block','marginTop':0});
-        $('#main .popup .popup-cont').css({'backgroundImage':url});
+        $('main .popup').css({'display':'block','marginTop':0});
+        $('main .popup .popup-cont').css({'backgroundImage':url});
         $('html,body').css({'overflow':'hidden'}).animate({scrollTop:0},0);
     }
 
 
-    $('#main .popup .popup-btn ul li').eq(0).click(function(){
-        $('#main .popup').css({'display':'none'})
+    $('main .popup .popup-btn ul li').eq(0).click(function(){
+        $('main .popup').css({'display':'none'})
         $('html,body').css({'overflow':'auto'})
     });
 
-    $('#main .popup .popup-btn ul li').eq(1).click(function(){
+    $('main .popup .popup-btn ul li').eq(1).click(function(){
 
         index--;
         let url = 'url(img/content'+index+'.jpg)';
-        $('#main .popup .popup-cont').css({'backgroundImage':url});
+        $('main .popup .popup-cont').css({'backgroundImage':url});
 
         if(index==1){
-            index = 73;
+            index = totalItems+1;
         }
     });
 
-    $('#main .popup .popup-btn ul li').eq(2).click(function(){
+    $('main .popup .popup-btn ul li').eq(2).click(function(){
 
         index++;
         let url = 'url(img/content'+index+'.jpg)';
-        $('#main .popup .popup-cont').css({'backgroundImage':url});
+        $('main .popup .popup-cont').css({'backgroundImage':url});
         
-        if(index==72){
+        if(index==totalItems){
             index = 0;
         }
     });
