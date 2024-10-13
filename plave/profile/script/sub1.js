612cd8645934f2f15627d022fc2a8aac98d44893
oh! sangmi <136715716+smoooss@users.noreply.github.com>
2024-10-13 20:13:30 +0900

    Add files via upload

--- plave/profile/script/sub1.js
+++ plave/profile/script/sub1.js
@@ -1,24 +1,24 @@
 $(document).ready(function(){
 
-    $('#main .contentwrap .menu ul li').click(menu);
+    $('main .container .menu ul li').click(menu);
     
     let index = 0
  
     function menu(){
         index = $(this).index()+1;
         $(this).css({'fontWeight':'700'}).siblings().css({'fontWeight':'500'})
-        $('#main .contentwrap .content .front ul li').eq(index-1).css({'display':'block'}).siblings().css({'display':'none'})
-        $('#main .contentwrap .textwrap .text.t'+index).css({'display':'block'}).siblings().css({'display':'none'})
+        $('main .container .content .front ul li').eq(index-1).css({'display':'block'}).siblings().css({'display':'none'})
+        $('main .container .textwrap .text.t'+index).css({'display':'block'}).siblings().css({'display':'none'})
     }
 
     a=$(window).width()
     if(a<500){
 
         $('.header .menu').click(function(){
-            $('#main .link').css({'bottom':'60px'})
+            $('main .link').css({'bottom':'60px'})
         });
         $('.close').click(function(){
-            $('#main .link').css({'bottom':'20px'})
+            $('main .link').css({'bottom':'20px'})
         });
     }
 
