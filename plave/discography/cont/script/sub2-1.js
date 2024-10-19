fbaf6ed891cea9c3da7bffe738c93b7dde6d0456
oh! sangmi <136715716+smoooss@users.noreply.github.com>
2024-10-19 21:20:22 +0900

    Add files via upload

--- plave/discography/cont/script/sub2-1.js
+++ plave/discography/cont/script/sub2-1.js
@@ -30,23 +30,10 @@ $(document).ready(function(){
     });
     
 
-    a=$('main .container .cont.c4 ul li > img').height()
-            
-    $('main .container .cont.c4 ul li').find('a').css({'height':a})
-        
-
     $('main .container .cont.c4 ul li').mouseover(function(){
         $(this).find('a').css({'opacity':'1'})
     }).mouseout(function(){
         $(this).find('a').css({'opacity':'0'})
     })
-
-    $(window).on('resize',function(){
-
-        b=$('main .container .cont.c4 ul li > img').height()
-
-        $('main .container .cont.c4 ul li').find('a').css({'height':b})
-        
-    });
 
 });
