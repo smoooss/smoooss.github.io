c32f3349e2707406f4e724aae5102657ffaa294e
oh! sangmi <136715716+smoooss@users.noreply.github.com>
2024-10-19 22:35:24 +0900

    Add files via upload

--- plave/profile/script/sub1.js
+++ plave/profile/script/sub1.js
@@ -12,7 +12,14 @@ $(document).ready(function(){
     }
 
     a=$(window).width()
-    if(a<500){
+
+    if(a>=500 && a<1400){
+
+        $('.close').click(function(){
+            $('main .link').css({'position':'absolute','bottom':'-20px'})
+        });
+
+    }if(a<500){
 
         $('.header .menu').click(function(){
             $('main .link').css({'bottom':'60px'})
