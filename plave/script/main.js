07da4281d1f0d54b4b6b3044274bd0bb92c4fc4d
oh! sangmi <136715716+smoooss@users.noreply.github.com>
2024-11-06 01:02:02 +0900

    Update main.js

--- plave/script/main.js
+++ plave/script/main.js
@@ -9,35 +9,64 @@ $(document).ready(function(){
         $('main .container .content .text').css({'opacity':'1'})
     }
 
-    $('.menu').click(function(){
-        $('html,body').css({'overflow':'hidden'})
-        $('nav').fadeIn(150)
-        $('main .link').css({'position':'fixed'})
+
+    $('.header .menu').click(function(){
+
+        $('html,body').animate({scrollTop:0},0).css({'overflow':'hidden'})
+        $('nav').fadeIn(150).addClass('on')
+        $('main .link').css({'bottom':'35px'})
         $('main .link .bl').css({'display':'block'})
         $('main .link .wh').css({'display':'none'})
-        $('footer').css({'position':'fixed'})
         $('footer .footer p').css({'color':'#333333'})
 
-        if(a>=500 && a<1400){
-            $('main .link').css({'bottom':'50px'})
+        a=$(window).width();
+        b=$('nav').hasClass('on')
+
+        if(a<1400){
+            $('main .link').css({'display':'flex','bottom':'50px'})
         }
-        if(a<500){
-            $('main .link').css({'display':'flex'})
+        if(a<700){
+            if(b==true){
+                $('main .link').css({'position':'fixed','bottom':'80px'})
+                $('footer').css({'position':'fixed'})
+            }
         }
     });
+
     $('.close').click(function(){
+        
         $('html,body').css({'overflow':'auto'})
-        $('nav').fadeOut(100)
+        $('nav').fadeOut(100).removeClass('on')
         $('main .link .bl').css({'display':'none'})
         $('main .link .wh').css({'display':'block'})
         $('footer').css({'position':'absolute'})
         $('footer .footer p').css({'color':'#ffffff'})
 
-        if(a>=500 && a<1400){
-            $('main .link').css({'position':'relative','bottom':'70px'})
-        }
+        a=$(window).width();
+
         if(a<500){
             $('main .link').css({'display':'none'})
+        }
+    });
+
+
+    $(window).resize(function(){
+
+        $('main .link').css({'position':'fixed','display':'block','bottom':'35px'})
+
+        a=$(window).width();
+        b=$('nav').hasClass('on')
+
+        if(a<1400){
+            $('main .link').css({'display':'flex','bottom':'50px'})
+        }
+        if(a<700){
+            if(b==true){
+                $('main .link').css({'position':'fixed','bottom':'80px'})
+            }
+            if(b==false){
+                $('main .link').css({'position':'relative','bottom':'100px'})
+            }
         }
     });
 
