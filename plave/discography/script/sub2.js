444e4706a6edde600dad87b2f265cbe8302a6667
oh! sangmi <136715716+smoooss@users.noreply.github.com>
2024-11-06 01:05:29 +0900

    Update sub2.js

--- plave/discography/script/sub2.js
+++ plave/discography/script/sub2.js
@@ -1,9 +1,8 @@
 $(document).ready(function(){
     
     $('main .container ul li').mouseover(function(){
-        a=$('main .container ul li').width()
-            
-            $(this).find('a').css({'height':a,'opacity':'1'})
+
+            $(this).find('a').css({'opacity':'1'})
         }).mouseout(function(){
             $(this).find('a').css({'opacity':'0'})
         })
