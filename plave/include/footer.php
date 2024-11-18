e886a1875ecc831543b5dda652022aa88fc87092
oh! sangmi <136715716+smoooss@users.noreply.github.com>
2024-11-18 14:37:48 +0900

    Add files via upload

---
+++ plave/include/footer.php
@@ -0,0 +1,10 @@
+            <footer>
+                <div class="footer">
+                    <p>Copyright ⓒ</p>
+                    <img src="https://smoooss.github.io/plave/img/footer/vlast_logo_rd.svg" alt="블래스트로고">
+                    <p>Co., Ltd. All rights reserved.</p>    
+                </div>
+            </footer>
+        </div>
+    </body>
+</html>
---
+++ plave/include/header.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+    <html lang="ko">
+        <head>
+            <meta charset="UTF-8">
+            <meta name="viewport" content="width=device-width, initial-scale=1.0">
+            <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+            <script src="https://smoooss.github.io/plave/script/sub.js"></script>
+            <script src="https://smoooss.github.io/plave/script/sub6.js"></script>
+            <link rel="stylesheet" href="https://smoooss.github.io/plave/css/reset.css">
+            <link rel="stylesheet" href="https://smoooss.github.io/plave/css/sub.css">
+            <link rel="stylesheet" href="https://smoooss.github.io/plave/css/sub6.css">
+            <link href="https://smoooss.github.io/plave/img/header/plave_bl.svg" rel="shortcut icon" type="image/x-icon">
+            <title>PLAVE</title>
+
+            <!-- Script -->
+            <?php require_once('include/script.php'); ?>
+
+            <!-- Style CSS -->
+            <?php require_once('include/style.php'); ?>
+        </head>
+    <body>
