ed98a2364144b14939dcd7f8bf64a36ef9396915
oh! sangmi <136715716+smoooss@users.noreply.github.com>
2025-01-21 17:07:21 +0900

    Add files via upload

---
+++ plave/include/header.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html>
+<html lang="ko">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+        <script src="https://smoooss.github.io/plave/resource/js/script.js"></script>
+        <link rel="stylesheet" href="https://smoooss.github.io/plave/resource/css/style.css">
+        <link href="https://smoooss.github.io/plave/resource/img/logo/plave_bl.svg" rel="shortcut icon" type="image/x-icon">
+        <title>PLAVE : Profile</title>
+    </head>
+    <body>
+        <div id="wrap">
+            <header>
+                <div class="header">
+                    <h1 class="logo">
+                        <a href="https://smoooss.github.io/plave/">
+                            <img src="https://smoooss.github.io/plave/resource/img/logo/plave_logo_bl.svg" alt="하얀색로고">
+                        </a>
+                    </h1>
+                    <span class="menu">
+                        <img src="https://smoooss.github.io/plave/resource/img/btn/menu_bl.png" alt="메뉴">
+                    </span>    
+                </div>
+            </header>
+        
+            <nav>
+                <div class="top">
+                    <a href="https://smoooss.github.io/plave/" class="logo">
+                        <img src="https://smoooss.github.io/plave/resource/img/logo/plave_logo_bl.svg" alt="검은색로고">
+                    </a>
+                    <span class="close">
+                        <img src="https://smoooss.github.io/plave/resource/img/btn/close_bl.png" alt="닫기">
+                    </span>    
+                </div>
+                <div class="nav">
+                    <ul>
+                        <li>
+                            <a href="https://smoooss.github.io/plave/profile/">PROFILE</a>
+                        </li>
+                        <li>
+                            <a href="https://smoooss.github.io/plave/discography/">DISCOGRAPHY</a>
+                        </li>
+                        <li>
+                            <a href="https://smoooss.github.io/plave/gallery/">GALLERY</a>
+                        </li>
+                        <li>
+                            <a href="https://smoooss.github.io/plave/video/show/">VIDEO</a>
+                        </li>
+                        <li>
+                            <a href="https://smoooss.github.io/plave/notice/2025/">NOTICE</a>
+                        </li>
+                        <li>
+                            <a href="https://smoooss.github.io/plave/schedule/">SCHEDULE</a>
+                        </li>
+                    </ul>
+                </div>
+            </nav>
