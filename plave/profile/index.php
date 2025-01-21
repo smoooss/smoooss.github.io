2895fd7b368ba244181cc31bacd16b4ce515ad10
oh! sangmi <136715716+smoooss@users.noreply.github.com>
2025-01-21 17:05:49 +0900

    Add files via upload

---
+++ plave/profile/index.php
@@ -0,0 +1,66 @@
+<? include "../resource/include/header.php";?>
+        
+                <main>
+                    <section class="container">
+                        <div class="menu">
+                            <ul>
+                                <li>YEJUN</li>
+                                <li>NOAH</li>
+                                <li>BAMBY</li>
+                                <li>EUNHO</li>
+                                <li>HAMIN</li>
+                            </ul>
+                        </div>
+                        <div class="content">
+                            <div class="back"></div>
+                            <div class="front">
+                                <ul>
+                                    <li></li>
+                                    <li></li>
+                                    <li></li>
+                                    <li></li>
+                                    <li></li>
+                                </ul>
+                            </div>
+                        </div>
+                        <div class="textwrap">
+                            <div class="text t1">
+                                <dt>YEJUN</dt>
+                                <dd>남예준 🐬💙</dd>
+                                <dd>2001. 09. 12</dd>
+                                <dd>리더, 메인보컬, 프로듀서</dd>
+                                <dd>ISTJ-T</dd>
+                            </div>
+                            <div class="text t2">
+                                <dt>NOAH</dt>
+                                <dd>한노아 🦙💜</dd>
+                                <dd>2001. 02. 10</dd>
+                                <dd>메인보컬, 프로듀서</dd>
+                                <dd>ISFJ-A</dd>
+                            </div>
+                            <div class="text t3">
+                                <dt>BAMBY</dt>
+                                <dd>채봉구(밤비) 🦌💗</dd>
+                                <dd>2002. 07. 15</dd>
+                                <dd>메인댄서, 보컬, 안무가</dd>
+                                <dd>INTP-A</dd>
+                            </div>
+                            <div class="text t4">
+                                <dt>EUNHO</dt>
+                                <dd>도은호 🐺❤️</dd>
+                                <dd>2003. 05. 24</dd>
+                                <dd>메인래퍼, 보컬, 프로듀서</dd>
+                                <dd>ENTP-A</dd>
+                            </div>
+                            <div class="text t5">
+                                <dt>HAMIN</dt>
+                                <dd>유하민 🐈‍⬛🖤</dd>
+                                <dd>????. 11. 01</dd>
+                                <dd>메인댄서, 리드래퍼, 안무가</dd>
+                                <dd>INFJ-A</dd>
+                            </div>    
+                        </div>
+                    </section>
+                </main>
+
+<? include "../resource/include/footer.php";?>
